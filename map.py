5d7af664c41a04663dd21f9c99e08594d83fe380
dremansley1 <44164463+dremansley1@users.noreply.github.com>
2018-10-16 11:35:57 +0100

    Add files via upload

--- map.py
+++ map.py
@@ -1,81 +1,120 @@
-from items import *
+cardiff_uni = {
 
-room_reception = {
-    "name": "Reception",
+    "name": "Cardiff University",
 
-    "description":
-    """You are in a maze of twisty little passages, all alike.
-Next to you is the School of Computer Science and
-Informatics reception. The receptionist, Matt Strangis,
-seems to be playing an old school text-based adventure
-game on his computer. There are corridors leading to the
-south and east. The exit is to the west.""",
+    "description": "",
 
-    "exits": {"south": "Admins", "east": "Tutor", "west": "Parking"},
-
-    "items": [item_biscuits, item_handbook]
+    "exits": {"east": "Blackweir Tavern", "west": "Bute Park", "south": "Library"}
+    
 }
 
-room_admins = {
-    "name": "MJ and Simon's room",
+blackweir_tavern = {
+     "name": "Blackweir Tavern",
 
-    "description":
-    """You are leaning agains the door of the systems managers'
-room. Inside you notice Matt "MJ" John and Simon Jones. They
-ignore you. To the north is the reception.""",
+    "description": "",
 
-    "exits":  {"north": "Reception"},
-
-    "items": []
+    "exits": {"east": "Lidl", "west": "Cardiff University"}
 }
 
-room_tutor = {
-    "name": "your personal tutor's office",
+bute_park = {
+     "name": "Bute Park",
 
-    "description":
-    """You are in your personal tutor's office. He intently
-stares at his huge monitor, ignoring you completely.
-On the desk you notice a cup of coffee and an empty
-pack of biscuits. The reception is to the west.""",
+    "description": "",
 
-    "exits": {"west": "Reception"},
-
-    "items": []
+    "exits": {"north": "Cardiff University", "east": "City Hall", "south": "Castle"}
 }
 
-room_parking = {
-    "name": "the parking lot",
+building_library = {
+    "name": "Library",
 
-    "description":
-    """You are standing in the Queen's Buildings parking lot.
-You can go south to the COMSC reception, or east to the
-general office.""",
+    "description": "",
 
-    "exits": {"east": "Office", "south": "Reception"},
-
-    "items": []
+    "exits": {"north": "Cardiff University", "west": "Lidl", "south": "City Hall"}
 }
 
-room_office = {
-    "name": "the general office",
+building_lidl = {
+    "name": "Lidl",
 
-    "description":
-    """You are standing next to the cashier's till at
-30-36 Newport Road. The cashier looks at you with hope
-in their eyes. If you go west you can return to the
-Queen's Buildings.""",
+    "description": "",
 
-    "exits": {"west": "Parking"},
-
-    "items": [item_pen]
+    "exits": {"west": "Library"}
 }
 
+city_hall = {
+    "name": "City Hall",
 
+    "description": "",
+
+    "exits": {"north": "Library", "east": "Student's Union", "west": "Bute Park"}
+}
+
+students_union = {
+    "name": "Student's Union",
+
+    "description": "",
+
+    "exits": {"north": "Lidl", "east": "Mystery Location", "south": "Museum", "west": "City Hall"}
+}
+
+building_castle = {
+    "name": "Castle",
+
+    "description": "",
+
+    "exits": {"north": "Bute Park", "east": "Museum", "west": "Mystery Location"}
+}
+building_museum = {
+    "name": "Museum",
+
+    "description": "",
+
+    "exits": {"east": "Student's Unions", "south": "Shopping Centre", "west": "Castle"}
+}
+
+principality_stadium = {
+    "name": "Principality Stadium",
+
+    "description": "",
+
+    "exits": {"north": "Castle", "east":"Museum"}
+}
+
+shopping_centre = {
+    "name": "Shopping Centre",
+
+    "description": "",
+
+    "exits": {"north": "Museum", "south":"unknown","east": "unknown", "west":"Principality Stadium"}
+}
+
+trap_door = {
+    "name": "Trap Door",
+
+    "description": "",
+
+    "exits": {"": ""}
+}
+
+safe_house = {
+    "name": "Safe House",
+
+    "description": "",
+
+    "exits": {"": ""}
+}
 
 rooms = {
-    "Reception": room_reception,
-    "Admins": room_admins,
-    "Tutor": room_tutor,
-    "Parking": room_parking,
-    "Office": room_office
+    "Cardiff Uni": cardiff_uni,
+    "Blackweir Tavern": blackweir_tavern,
+    "Bute Park": bute_park,
+    "Library": building_library,
+    "Lidl": building_lidl,
+    "City Hall": city_hall,
+    "Student's Union": students_union,
+    "Castle": building_castle,
+    "Museum": building_museum,
+    "Principality Stadium": principality_stadium,
+    "Shopping Centre": shopping_centre,
+    "Mystery Location": trap_door,
+    "Safe House": safe_house
 }

46bc0fa659ba5e7e381d4fdb83830503ef839e38
Steven Davies <stevenstdc@gmail.com>
2018-10-22 16:33:06 +0100

    Added names, ascii and event anmes and descriptions etc

--- events.py
+++ events.py
@@ -1,1 +1,190 @@
 #events
+Kirill_van = {
+	"ascii":
+	"""
+                                      ____.....-------.
+                      ___....-----````   __....-----.  `.
+            ___...---`    __......-----.| ___...---.|  / \
+         .``       _     | ___....----.|||        `|| /  `\
+        /_   ___....----.||          `||||.-.     `|||    `\
+       /  | |          `|||.-.       `||||   \    `|||  /  `\
+      |   | |.-.       `|||   \      `||||    \   `||| / `.)`\
+      |   | |   \      `|||    \     `||||___...---'|`--------`.
+     /    | |    \     `|||___....----'|| ____      |         .=\
+     |    | |___....----'| ____        ||(o)--'    [=]      ---.|
+    .`----'    _         |(o)--'     _ ||       _   |      _    |
+    |    _          _    |             |'-._        |           |
+    | __________________ | ___________ | ___'-.._  [=] ______  _/
+   /'.........._.---._ ...\ .......... | .._.--._`._|....... // \
+   \.'   _  .' _.-```-. `. |          / .'_.-```-. `.       ||.`.|
+  .--.     /.'  #      `. \|         | /.'     .  `. \   _   \\_/
+  |   \  _/.'    .--.  ` \||        / /.'  ` .--.   \ \     .---.
+   \   | |.' .  / _  |   ||'-------' |.' .  / _  | # ||     |   |
+    \__|_|'    | (_) / . |\__________/' #  | (_) /   |'.____\___|
+\   .'':'='    \    /    |  |         '    \    /    |  |
+  `.-'(.'  \ `  `--'    /  /           \    `--'  . /  /
+  /         \ #     #  /_.`             \ `   # .  /_.`
+             `.______.`                  `.______.'  
+	""",
+
+	"id": "Kirill_van",
+
+	"name": "Kirill's magical healing van",
+
+	"description": """Kirill finds you in his magical van!
+He asks you for your student ID,
+if you have it he says you shall be granted an item.
+If not he shall leave in a haste.
+You may not be given this opportunity again.
+	"""
+}
+Crazy_gang = {
+	"ascii":
+	"""
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMMMMMMds+:--------:+sdNMMMMMMMMMMM
+MMMMMMMMms:-+sdNMMMMMMMMNdy+--omMMMMMMMM
+MMMMMMh:` /mMMMMMMMMMMMMMMMMm+ `-yMMMMMM
+MMMMd--hN``--sNMMMMMMMMMMNy:..`md:.hMMMM
+MMM+`yMMMy hd+./hMMMMMMh/.+dd sMMMh`/MMM
+MM:.mMMMMM:.NMMh/.+dd+./hMMM--MMMMMm--NM
+M+`mMMMMMMN`+MMMMm-  .dMMMMo mMMMMMMN.:M
+d yMMMMMMMMy dNy:.omNs--sNm oMMMMMMMMh h
+/`MMMMMMMMMM.`.+dMMMMMMm+.``NMMMMMMMMM-:
+.:MMMMMMMd+./`oMMMMMMMMMMs /.+dMMMMMMM/`
+.:MMMMmo.:yNMs dMMMMMMMMm`oMNy:.omMMMM/`
+/`MNy:.omMMMMM--MMMMMMMM:.MMMMMNs--sNM.:
+d -` :++++++++: /++++++/ :++++++++:  : h
+M+ yddddddddddd+ yddddy /dddddddddddy`/M
+MM/.mMMMMMMMMMMM.-MMMM/.NMMMMMMMMMMm.:NM
+MMMo`sMMMMMMMMMMd sMMy hMMMMMMMMMMy`+MMM
+MMMMd--hMMMMMMMMM+`mN`/MMMMMMMMMh--hMMMM
+MMMMMMh:.omMMMMMMN.:/`NMMMMMMms.:hMMMMMM
+MMMMMMMMNs:./shmMMh  yMMNds/.:smMMMMMMMM
+MMMMMMMMMMMMdy+/---``---:+sdMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+
+	""",
+
+	"id": "Crazy_gang",
+
+	"name": "A crazy gang",
+
+	"description": """You encounter a crazy gang!
+They have a strange logo on their arm.
+You can either fight, flee or bribe.
+If you bribe without enough money, you will die.
+They take a bribe of £50.
+Fighting could be costly.
+	"""
+}
+Machete_murderer = {
+	"ascii":
+	"""          **
+                 |/__
+                 oo__\
+                 oo  \\
+                 oo   /
+                 oo
+            oooooooooooo
+            o  ^    ^  o
+            o  o    o  o
+            o   _||_   o
+            o   \__/   o
+/\           o        o           /\
+\ \           o  --  o           / /
+ \ \           o oo o           / /
+  \ \        o        o        / /
+   \ \      o          o      / /
+    \ \    o            o    / /
+     \ \__o              o__/ /
+     |-_                    _-|
+      \__                  __/
+         \                /
+          o``````````````o
+         o                o
+        o                  o
+       o                    o
+      o                      o
+     o~~~~~~~~~~~~~~~~~~~~~~~~o
+     o~~~~~~~~~~~~~~~~~~~~~~~~o
+         |  |          |  |
+         |  |          |  |
+         |  |          |  |
+       __|   \       __|   \
+      (_______]     (_______]
+	"""
+	"id": "Machete_murderer",
+
+	"name": "A dual weilding machete murderer",
+
+	"description": """You encounter a dual weilding machete murderer.
+It seems impossible to bribe him.
+He seems impossible to defeat.
+This might be your demise.
+	"""
+}
+Sneak_attack = {
+	"ascii": 
+	"""
+	              | |____________________________________________________
+ _ _ _ _ _ _ _| |                                                  .'`.
+|_|_|_|_|_|_|_| |------------------------------------------------.'****>
+`.            | |_______________________________________________.'***.'
+  `.        .'| |                                               `**'
+    `-.___.'  `-'                                              .'*`.
+                                                               `._.' .
+                                                               .   .'*`.
+                                                             .'*`. `._.'
+	""",
+	"id": "Sneak_attack",
+
+	"name": "A SNEAK ATTACK!",
+
+	"description": """A SNEAK ATTACK!
+You've been ganked!
+Taking a massive amount of damage.
+It seems like it could be fatal.
+	"""
+	#Left to bleed on the ground.
+	#If no bandage applied within two turns bleed to death - function yet to implement.
+}
+Random_item = {
+	"ascii":
+	"""
+	         ________
+        _jgN########Ngg_
+      _N##N@@""  ""9NN##Np_
+     d###P            N####p
+     "^^"              T####
+                       d###P
+                    _g###@F
+                 _gN##@P
+               gN###F"
+              d###F
+             0###F
+             0###F
+             0###F
+             "NN@'
+
+              ___
+             q###r
+              ""
+	""",
+
+	"id": "Random_item",
+
+	"name": "An item on the ground",
+
+	"description": """You see something on the ground,
+the appearance of it seems like an item you could use or pocket.
+You hope for the best as you reach down to pick it up.
+	"""
+}
+events = {
+	"1" : Kirill_van,
+	"2": Crazy_gang,
+	"3": Machete_murderer,
+	"4": Sneak_attack,
+	"5": Random_item
+}

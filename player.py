6f494735d8fdf17007e9276104ec9c7702cd3eb9
dremansley1 <mansleyat@cardiff.ac.uk>
2018-10-22 15:48:15 +0100

    Another Update

--- player.py
+++ player.py
@@ -1,7 +1,13 @@
 from items import *
 from map import rooms
 
+global health
+global money
+
 inventory = [item_id, item_laptop, item_money]
 
+health = 100
+money = 0
+
 # Start game at the reception
-current_room = rooms["Cardiff University"]
+current_room = rooms["Blackweir Tavern"]

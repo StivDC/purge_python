a90010eadd12b8a418e1c56e47daf5fb8ea5e028
Ciprian Chirila <chirilac@cardiff.ac.uk>
2018-10-24 15:59:50 +0100

    added the riddle direcotry

---
+++ riddles.py
@@ -0,0 +1,51 @@
+first_riddle = {
+    "id": "first",
+
+    "discription":"There is a dead man in the middle of a field, nothing is around him and there are no footprints of any sort. There is an unopened package next to him.\n How did he die?",
+    "hint": "HINT: As he approached the field he knew he was going to die.",
+    "answer": "Failed Parachute".lower(),
+}
+
+second_riddle = {
+    "id": "second",
+
+    "discription":"As I was going to the mall I met a man with seven wives, Each wive held two bags, Each bag held a mother cat, Each mother cat had six babies \n How many people were going to the mall?",
+    "hint": "",
+    "answer": "1".lower(),
+
+}
+
+third_riddle = {
+    "id": "third",
+
+    "discription":"A car's odometer shows 72927 miles, a palindromic number. What are the minimum miles you would need to travel to form another? (a palindrome can be read both forwards and backwards, like Kayak)",
+    "hint": "",
+    "answer": "110",
+}
+
+forth_riddle = {
+    "id": "forth",
+
+    "discription":"I am slim and tall, Many find me desirable and appealing. They touch me and I give a false good feeling. Once I shine in splendor, But only once and then no more. For many I am ""to die for"".\n What am I?",
+    "hint": "",
+    "answer": "A Cigarette",
+}
+
+riddles = {
+    "1": first_riddle,
+    "2": second_riddle,
+    "3": third_riddle,
+    "4": forth_riddle,
+    
+}
+#money
+#gun
+#knife
+#student card
+#first aid kit
+#bandage
+#key
+
+#inplement a function to drop a random item if the player wants to carry to many
+#the player would gets told on his second move
+
